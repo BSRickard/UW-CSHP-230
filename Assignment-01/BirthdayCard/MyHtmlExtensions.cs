71239c1f30841c99fba5fffbf3a39025275f5d74
BSRickard <HF202.Brian@eRickard.com>
2018-04-25 02:32:18 +0000

    Added Assignment-01 homework

---
+++ Assignment-01/BirthdayCard/MyHtmlExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BirthdayCard
+{
+    public static class MyHtmlExtensions
+    {
+        public static MvcHtmlString Include(this HtmlHelper helper, string srcPath)
+        {
+            string path = HttpContext.Current.Server.MapPath(srcPath);
+            return MvcHtmlString.Create(File.ReadAllText(path));
+        }
+    }
+}
